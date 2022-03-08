d72e92331ef9c41f1d1bc2b56bf35693548b59e7
enes taha sarı <ets.34.es@gmail.com>
2022-03-08 20:11:09 +0300

    added ejs

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 const express = require("express");
 
+const ejs = require("ejs");
 const app = express();
 
+//Template Engines
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(express.static("public"));
+
+//Routes
 app.get("/", (req, res) => {
-  res.status(200).send("<h1> Hello Dunyali</h1>");
+  res.status(200).render("index", {
+    page_name: "index",
+  });
 });
+app.get("/about", (req, res) => {
+  res.status(200).render("about", {
+    page_name: "about",
+  });
+});
+app.get("/contact", (req, res) => {
+  res.status(200).render("contact", {
+    page_name: "contact",
+  });
+});
+app.get("/pricing", (req, res) => {
+  res.status(200).render("pricing", {
+    page_name: "pricing",
+  });
+});
+app.get("/teachers", (req, res) => {
+  res.status(200).render("teachers", {
+    page_name: "teachers",
+  });
+});
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`ÏSunucu ${PORT} uzerinde calisiyor...`);
+  console.log(`Sunucu ${PORT} uzerinde calisiyor...`);
 });
